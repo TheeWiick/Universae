dcfacb62a2a6dfa922017777b038fc20c291a4da
JaviTM11 <167198794+JaviTM11@users.noreply.github.com>
2024-04-18 09:08:54 +0200

    iconos

--- src/main.java
+++ src/main.java
@@ -25,36 +25,132 @@ public class main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
+        ico1 = new javax.swing.JLabel();
+        ico2 = new javax.swing.JLabel();
+        ico3 = new javax.swing.JLabel();
+        ico4 = new javax.swing.JLabel();
+        ico5 = new javax.swing.JLabel();
+        ico6 = new javax.swing.JLabel();
+        ico7 = new javax.swing.JLabel();
+        ico8 = new javax.swing.JLabel();
+        ico9 = new javax.swing.JLabel();
+        ico10 = new javax.swing.JLabel();
+        ico11 = new javax.swing.JLabel();
+        ico12 = new javax.swing.JLabel();
+        ico13 = new javax.swing.JLabel();
+        ico14 = new javax.swing.JLabel();
+        bg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1920, 1080));
-        setPreferredSize(new java.awt.Dimension(1920, 1080));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1170, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 775, Short.MAX_VALUE)
-        );
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setOpaque(false);
+        jPanel1.setLayout(new java.awt.GridLayout(1, 14, 6, 0));
+
+        ico1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton8.png"))); // NOI18N
+        ico1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        ico1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ico1MouseClicked(evt);
+            }
+        });
+        jPanel1.add(ico1);
+
+        ico2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton0.png"))); // NOI18N
+        ico2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico2);
+
+        ico3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton1.png"))); // NOI18N
+        ico3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico3);
+
+        ico4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton2.png"))); // NOI18N
+        ico4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico4);
+
+        ico5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton3.png"))); // NOI18N
+        ico5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico5);
+
+        ico6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton4.png"))); // NOI18N
+        ico6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico6);
+
+        ico7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton5.png"))); // NOI18N
+        ico7.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico7);
+
+        ico8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton6.png"))); // NOI18N
+        ico8.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico8);
+
+        ico9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton7.png"))); // NOI18N
+        ico9.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico9);
+
+        ico10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton13.png"))); // NOI18N
+        ico10.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico10);
+
+        ico11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton9.png"))); // NOI18N
+        ico11.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico11);
+
+        ico12.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton10.png"))); // NOI18N
+        ico12.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico12);
+
+        ico13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton11.png"))); // NOI18N
+        ico13.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico13);
+
+        ico14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ico14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/LauncherButton12.png"))); // NOI18N
+        ico14.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(ico14);
+
+        jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 70, 1550, 130));
+
+        bg.setBackground(new java.awt.Color(0, 0, 0));
+        bg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fondo/FondoBase.png"))); // NOI18N
+        bg.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jPanel2.add(bg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1920, 1080));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ico1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ico1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ico1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -92,6 +188,22 @@ public class main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel bg;
+    private javax.swing.JLabel ico1;
+    private javax.swing.JLabel ico10;
+    private javax.swing.JLabel ico11;
+    private javax.swing.JLabel ico12;
+    private javax.swing.JLabel ico13;
+    private javax.swing.JLabel ico14;
+    private javax.swing.JLabel ico2;
+    private javax.swing.JLabel ico3;
+    private javax.swing.JLabel ico4;
+    private javax.swing.JLabel ico5;
+    private javax.swing.JLabel ico6;
+    private javax.swing.JLabel ico7;
+    private javax.swing.JLabel ico8;
+    private javax.swing.JLabel ico9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
